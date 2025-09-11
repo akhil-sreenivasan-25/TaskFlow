506518ba50d49d6775f85803531ac50734203bec
akhil sreenivasan <akhilsreenivasan7025@gmail.com>
2025-09-11 15:52:08 +0530

    template files added for login and a dashboard

--- taskflow/env1/taskflow/baseapp/urls.py
+++ taskflow/env1/taskflow/baseapp/urls.py
@@ -20,4 +20,5 @@ from . import views
 
 urlpatterns = [
     path('test/',views.test,name="test"),
+    path('login/',views.login,name="login"),
 ]
--- taskflow/env1/taskflow/baseapp/views.py
+++ taskflow/env1/taskflow/baseapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def test(request):
-    return render(request,"member dashboard.html")
+    return render(request,"member_dashboard.html")
+
+def login(request):
+    return render(request,"log_page.html")
