eda3c058875036c48a021af32e4dd5cdff39ba75
akhil sreenivasan <akhilsreenivasan7025@gmail.com>
2025-09-17 18:30:31 +0530

    role based login set

--- taskflow/env1/taskflow/baseapp/views.py
+++ taskflow/env1/taskflow/baseapp/views.py
@@ -12,11 +12,21 @@ def login_page(request):
         password=request.POST.get("password")
         user=authenticate(request,username=username,password=password)
         if user is not None:
-            login(request,user)
-            return render(request,"tl_dashboard.html")
+            catogry=user.desig_prefix
+            if catogry=="TL":
+                login(request,user)
+                return render(request,"tl_dashboard.html")
+            elif catogry=="TM":
+                login(request,user)
+                return render(request,"member_dashboard.html")
+            else:
+                return HttpResponse("Unauthorized user")
         else:
-            return HttpResponse("Invalid user")
-    return render(request,"log_page.html")
+            return HttpResponse("No user found")
+    else:
+        return render(request,"log_page.html")
+    
+    
 
 def tl_home(request):
     return  render(request,"tl_dashboard.html")
